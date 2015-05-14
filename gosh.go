00d1ef12ca06d89e02bcf65cb35d9abf18e3f6d6
Madis Lõhmus <madis@toggl.com>
2015-05-14 16:07:31 +0300

    Revert "Externalised getting session"
    This reverts commit 15f4550c71ebda745f3f00d1321bbeb3634aef4e.

--- gosh.go
+++ gosh.go
@@ -67,48 +67,33 @@ func GetSigner(keyFile, password string) (*ssh.Signer, error) {
 	return &signer, nil
 }
 
-func GetSession(cfg Config) (*ssh.Session, error) {
-	config := &ssh.ClientConfig{
-		User: cfg.User,
-		Auth: []ssh.AuthMethod{ssh.PublicKeys(*cfg.Signer)},
-	}
-	client, err := ssh.Dial("tcp", cfg.Host+":"+cfg.Port, config)
-	if err != nil {
-		return nil, err
-	}
-	session, err := client.NewSession()
-	if err != nil {
-		return nil, err
-	}
-	return session, nil
-}
-
-func RunOnSession(session *ssh.Session, command string) (string, error) {
-	var b bytes.Buffer
-	session.Stdout = &b
-	if err := session.Run(command); err != nil {
-		return "", err
-	}
-	return b.String(), nil
-}
-
 func Run(command string, cfg Config) (string, error) {
 	resChan := make(chan string)
 	errChan := make(chan error)
 	go func() {
+		config := &ssh.ClientConfig{
+			User: cfg.User,
+			Auth: []ssh.AuthMethod{ssh.PublicKeys(*cfg.Signer)},
+		}
+		client, err := ssh.Dial("tcp", cfg.Host+":"+cfg.Port, config)
+		if err != nil {
+			errChan <- err
+			return
+		}
 
-		session, err := GetSession(cfg)
+		session, err := client.NewSession()
 		if err != nil {
 			errChan <- err
 			return
 		}
 		defer session.Close()
-		output, err := RunOnSession(session, command)
-		if err != nil {
+		var b bytes.Buffer
+		session.Stdout = &b
+		if err := session.Run(command); err != nil {
 			errChan <- err
 			return
 		}
-		resChan <- output
+		resChan <- b.String()
 	}()
 	timeout := time.After(cfg.Timeout)
 	select {
